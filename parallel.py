53645804da237a30b7b3b62f23cb4ab66543b7bf
Kang Li <likangry03@hotmail.com>
2020-04-27 20:20:53 +0800

    adapt to the latest tvm

--- parallel.py
+++ parallel.py
@@ -1,13 +1,14 @@
 import tvm
+from tvm import te
 n = 1024
 m = 1024
 
-A = tvm.placeholder((n, m), name='A')
-l = tvm.reduce_axis((0, m), name = 'l')
+A = te.placeholder((n, m), name='A')
+l = te.reduce_axis((0, m), name = 'l')
 
-B = tvm.compute((n,), lambda i: tvm.sum(A[i, l], axis=l), name='B')
+B = te.compute((n,), lambda i: te.sum(A[i, l], axis=l), name='B')
 
-s = tvm.create_schedule(B.op)
+s = te.create_schedule(B.op)
 
 print(tvm.lower(s, [A, B], simple_mode=True))
 print("---------cutting line---------")
