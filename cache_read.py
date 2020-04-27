38cf80d5263a7f1576a0c1bced5c7247edec4883
Kang Li <likangry03@hotmail.com>
2020-04-27 17:40:08 +0800

    adapt to the latest tvm

--- cache_read.py
+++ cache_read.py
@@ -1,12 +1,12 @@
 import tvm
-
+from tvm import te
 n = 1024
 dtype = "float32"
-A = tvm.placeholder((n, n), dtype=dtype, name='A')
-k = tvm.reduce_axis((0, n), name='k')
-B = tvm.compute((n,), lambda i: tvm.sum(A[i, k], axis=k), name='B')
+A = te.placeholder((n, n), dtype=dtype, name='A')
+k = te.reduce_axis((0, n), name='k')
+B = te.compute((n,), lambda i: te.sum(A[i, k], axis=k), name='B')
 
-s = tvm.create_schedule(B.op)
+s = te.create_schedule(B.op)
 
 print(tvm.lower(s, [A, B], simple_mode=True))
 print("---------cutting line---------")
